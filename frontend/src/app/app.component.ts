c4fb9a16672dc348f42e29914548428f9af42d82
mfquinonesc <manuelfqc@gmail.com>
2025-06-04 08:31:44 -0500

    Update animations

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'frontend';
+
+  ngOnInit(): void {
+    AOS.init({
+      duration: 2000, // animation duration
+      once: true,     // whether animation should happen only once
+      
+    });
+  }
+ 
 }
+
